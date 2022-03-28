1c64072dc6604273ba81862cce8cdf8d1b96c4d8
Samadhi Chandrasena <samadhi.chandrasena@gmail.com>
2022-03-28 15:05:04 +1030

    changed tester makefile so that IT copies libft.o and libft.h into the tester folder

--- tester/strlen_test.c
+++ tester/strlen_test.c
@@ -5,7 +5,7 @@ int main()
 {
     int length;
     length = 0;
-    char string_to_check[] = "Samadhi";
+    char string_to_check[] = "samadhi";
     length = ft_strlen(string_to_check);
     printf("string length is %d", length);
     return 0;
